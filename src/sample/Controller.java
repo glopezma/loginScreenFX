364c850ae7ec2fbfb1bc527dbdee2a06e89e46a7
Gabriel Lopez <galopezmatthews@gmail.com>
2017-10-26 14:58:05 -0400

    Everything works.
    Finished other than the when the scene changes, it doesn't remove the previous one.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,40 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
 public class Controller {
+    @FXML
+    private Label status;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    public void Login(ActionEvent event) throws Exception{
+        if (username.getText().equals("user") && password.getText().equals("pass")) {
+            status.setText("Login Success");
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+            primaryStage.setTitle("Login");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } else {
+            status.setText("Login Failed");
+        }
+    }
+
 }
